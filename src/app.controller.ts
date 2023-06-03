e8bac87f495f960d35cc4a665b3110cd0e556bf5
marie <marie@profitintelligence.net>
2023-06-03 12:36:25 +0200

    added mitigationQuestionsServices class extended from environmentQuestionsService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { EnvironmentQuestionsService } from './environmentQuestionsService';
+import { MitigationQuestionsService } from "./mitigationQuestionsService";
 
 @Controller()
 export class AppController {
   constructor(
     private readonly environmentQuestionsService: EnvironmentQuestionsService,
+    private readonly mitigationQuestionsService: MitigationQuestionsService,
   ) {}
 
   @Get('environment_questions')
   getEnvironmentQuestions(): any {
     return this.environmentQuestionsService.getQuestions();
+  }
+
+  @Get('mitigation_questions')
+  getMitigationQuestions(): any {
+    return this.mitigationQuestionsService.getQuestions();
   }
 }
